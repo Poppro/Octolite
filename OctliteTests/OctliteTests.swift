7dde161bed3c5c5755bf368f39bf7fb43a952573
hunter harloff <hunterharloff@gmail.com>
2015-11-26 20:22:52 -0500

    Initial Commit

---
+++ OctliteTests/OctliteTests.swift
@@ -0,0 +1,36 @@
+//
+//  OctliteTests.swift
+//  OctliteTests
+//
+//  Created by Pop Pro on 11/26/15.
+//  Copyright © 2015 Poppro. All rights reserved.
+//
+
+import XCTest
+@testable import Octlite
+
+class OctliteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
